1423e15195f13c7f7ee303a53038026349f7c9bc
Henning Klein <henning.klein@klein-it.solutions>
2026-04-23 20:55:12 +0200

    move from nerzal to henning-kln namespace

--- client.go
+++ client.go
@@ -19,7 +19,7 @@ import (
 	"github.com/segmentio/ksuid"
 	"golang.org/x/mod/semver"
 
-	"github.com/Nerzal/gocloak/v13/pkg/jwx"
+	"github.com/henning-kln/gocloak/pkg/jwx"
 )
 
 // GoCloak provides functionalities to talk to Keycloak.
